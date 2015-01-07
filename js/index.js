bde4168386d85ce80cc7fcd2e641daee102dc15d
RCK <rockers7414@icloud.com>
2015-01-08 00:04:04 +0800

    加上家庭成員表格、新增資料輸入檢驗機制(未完成)。

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-$('#member li').click(function(){
+$('#member li').click(function() {
 	$('#member-value').text(this.children[0].text).append('<span class="caret"></span>');
 });
 
-$('#job li').click(function(){
+$('#job li').click(function() {
 	$('#job-value').text(this.children[0].text).append('<span class="caret"></span>');
+	switch(this.children[0].text) {
+		case '在職':
+			$('#salary-value').attr('disabled', false);
+			break;
+		default:
+			$('#salary-value').attr('disabled', true);
+	}
 });
+
+$('#addmember').click(function() {
+	var age = new Date().getFullYear() - (validateDigit($('#birthday-value').val()) ? 1911 + parseInt($('#birthday-value').val()) : new Date().getFullYear());
+	if (age == 0) {
+		$('#birthday').addClass('has-error');
+		$('#errormsg').text('出生年次填寫錯誤！').show();
+		return false;
+	}
+});
+
+function validateDigit(value) {
+	var re = /[0-9]+/;
+	return re.test(value);
+}
